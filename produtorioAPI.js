0e43efd45b0c0a7600269d65310368bf17bbcde7
Joker <victorassagra@hotmail.com>
2023-04-26 14:38:26 -0300

    melhoria na API

--- produtorioAPI.js
+++ produtorioAPI.js
@@ -2,21 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-function produtorio(m) {
-  let r = 1;
-  for (let i = 0; i < m; i++) {
-    i += 1;
-    r = r * ((i + 1) / i);
+function produtorio(m, n, recursive = false) {
+  if (recursive) {
+    if (n >= m) {
+      return 1;
+    }
+    return ((n + 1) / n) * produtorio(m, n + 1, recursive);
+  } else {
+    let r = 1;
+    for (let i = n; i < m + n; i++) {
+      r = r * ((i + 1) / i);
+    }
+    return r;
   }
-  return {
-    result: r
-  };
 }
 
-app.get('/produtorio/:m', (req, res) => {
+app.get('/produtorio/:m/:n/:recursive?', (req, res) => {
   const m = parseInt(req.params.m);
-  const result = produtorio(m);
-  res.json(result);
+  const n = parseInt(req.params.n);
+  const recursive = req.params.recursive === 'true';
+  const result = produtorio(m, n, recursive);
+  res.json({ result });
 });
 
 app.listen(port, () => {
